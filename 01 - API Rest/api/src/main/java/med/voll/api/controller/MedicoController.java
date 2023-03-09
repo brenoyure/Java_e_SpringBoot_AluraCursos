302908d25c5ca762e7f4a1eba2b2e54ffd8ea473
Breno Yuri <byurifb@gmail.com>
2023-03-09 16:42:55 -0300

    Paginação & Ordenação no cadastro de Médicos
    Implementada a opção de paginação e ordenação na controller de médicos.

--- 01 - API Rest/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ 01 - API Rest/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,8 +1,9 @@
 package med.voll.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,8 +31,8 @@ public class MedicoController {
 	}
 
 	@GetMapping
-	public List<DadosListagemMedico> listar() {
-		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+		return repository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
 
 }
